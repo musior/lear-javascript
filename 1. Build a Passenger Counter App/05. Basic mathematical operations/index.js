ad0cd5b5dc8b6f1dc1582163add9bfc21f26cf0c
Mateusz <108770502+musior@users.noreply.github.com>
2023-11-05 10:42:42 +0100

    Update index.js

--- 1. Build a Passenger Counter App/05. Basic mathematical operations/index.js
+++ 1. Build a Passenger Counter App/05. Basic mathematical operations/index.js
@@ -1,13 +1,16 @@
 // document.getElementById("count").innerText = 5
 
 // cmd+k+c
-// let firstBatch = 5
-// let secondBatch = 7
-// let count = firstBatch + secondBatch
-// console.log(count)
+let firstBatch = 5
+let secondBatch = 7
+let count = firstBatch + secondBatch
+console.log(count)
 
 
 // 1. Create two variables, myAge and humanDogRatio
+let myAge = 30;
+let humanDogRatio = 7;
 // 2. Multiply the two together and store the result in myDogAge 
+let myDogAge = myAge * humanDogRatio;
 // 3. Log myDogAge to the console
-
+console.log(myDogAge);
