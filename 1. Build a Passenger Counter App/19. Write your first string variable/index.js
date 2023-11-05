09292f4bca52d0fb5b8015e2abf9520c61c969a5
Mateusz <108770502+musior@users.noreply.github.com>
2023-11-05 16:14:09 +0100

    Update index.js

--- 1. Build a Passenger Counter App/19. Write your first string variable/index.js
+++ 1. Build a Passenger Counter App/19. Write your first string variable/index.js
@@ -1,4 +1,10 @@
-let username = 'per"
+let username = 'per';
+let message = 'You have tree new notifications';
 
 console.log(username)
+console.log(message);
+console.log(message + ", " + username + "!");
 
+let messageToUser = message + ", " + username + "!";
+
+console.log(messageToUser);
