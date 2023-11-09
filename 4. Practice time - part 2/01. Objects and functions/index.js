69e61310f1620efd5c7674d8afb46eab432e9f38
Mateusz <108770502+musior@users.noreply.github.com>
2023-11-09 19:03:30 +0100

    Done tasks

--- 4. Practice time - part 2/01. Objects and functions/index.js
+++ 4. Practice time - part 2/01. Objects and functions/index.js
@@ -6,3 +6,14 @@
 // "Per is 35 years old and lives in Norway"
 
 // Call the logData() function to verify that it works
+let person = {
+  name: 'Mateusz',
+  age: 30,
+  country: 'Poland'
+}
+
+function logData() {
+  console.log(`${person.name} is ${person.age} years old and lives in ${person.country}`);
+}
+
+logData();
